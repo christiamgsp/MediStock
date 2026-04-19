e6a155277eee6626e21055cc696393aa4147e075
Briel <christiamgsp@gmail.com>
2026-04-19 09:14:40 +0200

    feat: implementar lista de productos y métodos de búsqueda en StockService

--- src/app/services/stock.ts
+++ src/app/services/stock.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Medicamento } from '../models/medicamento';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Stock {}
+export class Stock {
+  listaMedicamentos: Medicamento[] = [
+    { id: 1, nombre: 'Paracetamol', precio: 5, stock: 20 },
+    { id: 2, nombre: 'Ibuprofeno', precio: 8, stock: 15 },
+    { id: 3, nombre: 'Amoxicilina', precio: 12, stock: 10 },
+  ];
+  constructor() {}
+
+  buscarPorId = (idBuscado: number) => this.listaMedicamentos.find((m) => m.id === idBuscado);
+
+  obtenerTotalProductos = () => this.listaMedicamentos.length;
+}
