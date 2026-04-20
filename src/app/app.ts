dfbf1eeb943a3851d013895a351f61f42d2b6b52
Briel <christiamgsp@gmail.com>
2026-04-20 09:51:16 +0200

    feat: listar medicamentos en el AppComponent usando *ngFor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './shared/components/navbar/navbar';
+import { Stock } from './services/stock';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Stock],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('medistock');
