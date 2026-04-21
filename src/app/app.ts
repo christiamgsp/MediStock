c4fe3e3bf01a7df601f4d634d37475725d4d9458
Briel <christiamgsp@gmail.com>
2026-04-21 10:23:52 +0200

    feat: implementar FormsModule y primer ejemplo de vinculación bidireccional con ngModel

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/components/navbar/navbar';
 import { Stock } from './services/stock';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule],
+  imports: [RouterOutlet, Navbar, CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
+  textoBusqueda: string = '';
   constructor(public stock: Stock) {}
 }
