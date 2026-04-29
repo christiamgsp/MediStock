fe383e5725926a2fa7a2ce6493e898f69bf68378
christiamgsp <christiamgsp@gmail.com>
2026-04-29 08:06:36 +0200

    fix: restaurar función obtenerTotalProductos y limpiar buscador en el template

--- src/app/services/stock.ts
+++ src/app/services/stock.ts
@@ -7,8 +7,13 @@ import { Medicamento } from '../models/medicamento';
 export class Stock {
   listaMedicamentos: Medicamento[] = [
     { id: 1, nombre: 'Paracetamol', precio: 5, stock: 20 },
-    { id: 2, nombre: 'Ibuprofeno', precio: 8, stock: 15 },
+    { id: 2, nombre: 'Ibuprofeno', precio: 8, stock: 4 },
     { id: 3, nombre: 'Amoxicilina', precio: 12, stock: 10 },
+    { id: 4, nombre: 'Omeprazol', precio: 15, stock: 25 },
+    { id: 5, nombre: 'Aspirina', precio: 6, stock: 2 },
+    { id: 6, nombre: 'Loratadina', precio: 10, stock: 15 },
+    { id: 7, nombre: 'Metformina', precio: 20, stock: 30 },
+    { id: 8, nombre: 'Vitamina C', precio: 18, stock: 45 },
   ];
 
   constructor() {
